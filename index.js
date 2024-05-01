23e63f59d5d66d729bd2e9c6afb393ac1c10f2b5
candourist <sajadh.ismael@gmail.com>
2024-05-01 16:30:38 +0300

    Completed assignment OwO

---
+++ index.js
@@ -0,0 +1,10 @@
+let divide = function () {
+    return 2000 / 100
+}
+console.log(divide())
+
+let square = (num1) => num1 * num1
+console.log(square())
+
+let add = (num1, num2) => num1 + num2
+console.log(add(1,2))
